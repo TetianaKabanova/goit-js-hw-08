ba98c362ec85285a1deda3988c4228a42ae7e943
TetianaKabanova <110104725+TetianaKabanova@users.noreply.github.com>
2023-04-17 20:11:25 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,9 +2,10 @@ import throttle from 'lodash.throttle';
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  email: document.querySelector('input'),
-  message: document.querySelector('textarea'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  input: document.querySelector('input'),
 };
+
 const STORAGE_KEY = 'feedback-form-state';
 
 let feedbackForm = {
@@ -13,15 +14,18 @@ let feedbackForm = {
 };
 
 populateTextarea();
+
 reloadPage();
+
 refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 refs.form.addEventListener('submit', onFormSubmit);
 
 function reloadPage() {
   if (feedbackForm) {
-    refs.email.value = feedbackForm.email || '';
-    refs.message.value = feedbackForm.message || '';
+    refs.input.value = feedbackForm.email || '';
+    refs.textarea.value = feedbackForm.message || '';
   }
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function onTextareaInput(event) {
@@ -32,23 +36,20 @@ function onTextareaInput(event) {
 
 function onFormSubmit(event) {
   event.preventDefault();
-
+  console.log(feedbackForm);
   localStorage.removeItem(STORAGE_KEY);
   event.currentTarget.reset();
-  feedbackForm.email = refs.email.value;
-  feedbackForm.message = refs.message.value;
-  console.log(feedbackForm);
+  feedbackForm = {};
 }
 
 function populateTextarea() {
   const savedMessage = localStorage.getItem(STORAGE_KEY);
-  const objectValues = JSON.parse(savedMessage);
-
-  if (objectValues) {
-    feedbackForm = objectValues;
-    refs.email.value = objectValues.email || '';
-    refs.message.value = objectValues.message || '';
-    feedbackForm = objectValues.email || '';
-    feedbackForm = objectValues.message || '';
+  if (savedMessage === null) {
+    return;
   }
+
+  refs.textarea.value = savedMessage.message || '';
+  refs.input.value = savedMessage.email || '';
+  feedbackForm.email = savedMessage.email || '';
+  feedbackForm.message = savedMessage.message || '';
 }
